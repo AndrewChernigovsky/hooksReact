cbe8e5ae54f957fcf889ec8d229a0d5f798d4223
Andrew Izosimov <chernigovsky108@gmail.com>
2024-06-22 21:48:56 +0300

    fixies text orphagraphy

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
 export const Header = ({ title, src }) => {
-	return (
-		<>
-			<h2>{title}</h2>
-			<span>Код этого файла можно найти в файле:</span> <code>{src}</code>
-		</>
-	);
+  return (
+    <>
+      <h2>{title}</h2>
+      <span>Код этого файла можно найти в файле:</span> <code>{src}</code>
+    </>
+  );
 };
--- src/useState/examples/UseStateExample9.jsx
+++ src/useState/examples/UseStateExample9.jsx
@@ -3,66 +3,66 @@ import { Link } from "react-router-dom";
 import { Header } from "../../Header.jsx";
 
 export const UseStateExample9 = ({ title, srcPath, nextPageUrl }) => {
-	const [languagesInfo, setLanguagesInfo] = useState([
-		{
-			name: "TypeScript",
-			createdAt: "2012",
-			shortName: "TS",
-		},
-		{
-			name: "JavaScript",
-			createdAt: "1996",
-			shortName: "CSS",
-		},
-	]);
+  const [languagesInfo, setLanguagesInfo] = useState([
+    {
+      name: "TypeScript",
+      createdAt: "2012",
+      shortName: "TS",
+    },
+    {
+      name: "JavaScript",
+      createdAt: "1996",
+      shortName: "CSS",
+    },
+  ]);
 
-	const changeValue = () => {
-		const correctArray = languagesInfo.map((language) => {
-			if (language.name === "JavaScript") {
-				return { ...language, shortName: "JS" };
-			}
-			return language;
-		});
+  const changeValue = () => {
+    const correctArray = languagesInfo.map((language) => {
+      if (language.name === "JavaScript") {
+        return { ...language, shortName: "JS" };
+      }
+      return language;
+    });
 
-		setLanguagesInfo(correctArray);
-	};
+    setLanguagesInfo(correctArray);
+  };
 
-	return (
-		<div>
-			<Header title={title} src={srcPath} />
-			<p>
-				Давай снова отредактируем наш немного не правильный массив объектов с
-				языками
-			</p>
-			<p>Короткое имя для JavaScript указано не верно и мы хоти его поменять</p>
-			<p>Нажми на кнопку и убедись что оно изменилось на верное</p>
+  return (
+    <div>
+      <Header title={title} src={srcPath} />
+      <p>
+        Давай снова отредактируем наш немного не правильный массив объектов с
+        языками
+      </p>
+      <p>Короткое имя для JavaScript указано неверно и мы хотим его поменять</p>
+      <p>Нажми на кнопку и убедись, что оно изменилось на верное</p>
 
-			<button onClick={changeValue} style={{ marginBottom: "10px" }}>
-				Change value
-			</button>
+      <button onClick={changeValue} style={{ marginBottom: "10px" }}>
+        Change value
+      </button>
 
-			{languagesInfo.map((language) => {
-				return (
-					<div key={language.name}>
-						<code>name: {language.name};</code>
-						<code>createdAt: {language.createdAt};</code>
-						<code>shortName: {language.shortName};</code>
-					</div>
-				);
-			})}
-			<p>Что здесь произошло?</p>
-			<p>
-				Зная то, что метод <code>map</code> возвращает на новый массив мы
-				используем его, для перебора элементов массива
-			</p>
-			<p>
-				Внутри мы проверяем, <code>if(language.title === "JavaScript")</code>{" "}
-				если это так, то мы возвращаем новый объект с обновленным полем{" "}
-				<code>{'return { ...language, shortName: "JS" }'}</code>
-				Если же это не тот объект который нам нужен, мы его возвращаем в
-				исходном виде
-			</p>
-			<Link to={nextPageUrl}>Начать практиковаться</Link>
-		</div>
-	);
+      {languagesInfo.map((language) => {
+        return (
+          <div key={language.name}>
+            <code>name: {language.name};</code>
+            <code>createdAt: {language.createdAt};</code>
+            <code>shortName: {language.shortName};</code>
+          </div>
+        );
+      })}
+      <p>Что здесь произошло?</p>
+      <p>
+        Зная то, что метод <code>map</code> возвращает нам новый массив мы
+        используем его, для перебора элементов массива
+      </p>
+      <p>
+        Внутри мы проверяем, <code>if(language.title === "JavaScript")</code>{" "}
+        если это так, то мы возвращаем новый объект с обновленным полем{" "}
+        <code>{'return { ...language, shortName: "JS" }'}</code>
+        Если же это не тот объект который нам нужен, мы его возвращаем в
+        исходном виде
+      </p>
+      <Link to={nextPageUrl}>Начать практиковаться</Link>
+    </div>
+  );
 };
